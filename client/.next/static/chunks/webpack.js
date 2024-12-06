50f2d20ec888787693217bea713170fbf4346f3d
ramya <nsriramya7@gmailcom>
2024-12-06 23:04:20 +0530

    fixed errors

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c0423fbeb2eea69"; }
+/******/ 		__webpack_require__.h = function() { return "b0cbfc61cba4e13c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React,{useEffect,useRef} from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({options,cordinates,ContextMenu,setContextMenu}) {
+  const contextMenuRef= useRef(null);
+
+
+  useEffect(()=>{
+    const handleOutsideClick=(event)=>{
+      if(event.target.id!=="context-opener"){
+        if(contextMenuRef.current && !contextMenuRef.current.contains(event.target)){
+          setContextMenu(false);
+        }
+
+      }
+    };
+    document.addEventListener("click",handleOutsideClick);
+    return()=>{
+      document.removeEventListener("click",handleOutsideClick);
+
+    };
+
+  },[]);
+  const handleClick =(e,callback)=>{
+    e.stopPropagation();
+    setContextMenu(false);
+    callback();
+  };
+  return <div
+   className={'bg-dropdown-background fixed py-2 z-[100]top-[${cordinates.y} left-[${cordinates.x}] shadow-xl'}
+
+  ref={contextMenuRef}
+  style={{
+    top:cordinates.y,
+    left:cordinates.x,
+    // top-[${cordinates.y}] left-[${cordinates.x}]
+  }}
+   >
+    <ul>
+      {options.map(({name,callback})=>(
+          <li key={name} onClick={()=>handleClick(e,callback)}
+          className="px-5 py-2 cursor-pointer hover:bg-background-default">
+
+            <span className="text-white">{name}</span></li>
+        ))
+      }
+    </ul>
+    ContextMenu
+  </div>;
 }
 
 export default ContextMenu;
